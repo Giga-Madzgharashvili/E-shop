f97ef5c36bc25f2b4ff2a564b67ed94736a7bbc8
giga <gmadzgharashvili@gmail.com>
2022-08-21 22:20:21 +0400

    add product section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.sl').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    // centerMode: true,
+//   variableWidth: true
+});
