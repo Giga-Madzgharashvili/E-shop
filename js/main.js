c7c8fdd796189b3a1c2561154574addb0b7789b7
giga <gmadzgharashvili@gmail.com>
2022-08-23 23:21:21 +0400

    add contac section & scrol down effect

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $('.sl').slick({
     dots: false,
-    infinite: false,
-    speed: 300,
+    infinite: true,
+    // speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    fade: true,
+    cssEase: 'linear'
+    // dots: true
     // centerMode: true,
 //   variableWidth: true
 });
+
+// scrol section
+$('.js-anchor-link').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+      var scrollTo = target.offset().top;
+      $('body, html').animate({scrollTop: scrollTo+'px'}, 1500);
+    }
+  });
