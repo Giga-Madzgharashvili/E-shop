951da545ad8deff7b5d0af01064fd18b1897227f
giga <gmadzgharashvili@gmail.com>
2022-08-28 18:26:08 +0400

    finish

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 $(".sl").slick({
   dots: false,
   infinite: true,
-  // speed: 300,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
   fade: true,
   cssEase: "linear",
-  // dots: true
-  // centerMode: true,
-  //   variableWidth: true
+  
 });
 
 // scrol section
@@ -27,5 +24,20 @@ let toggleButton = document.getElementById('burger');
 let navigation = document.getElementById('nav');
 toggleButton.addEventListener('click', function() {
   navigation.classList.toggle('nav-active');
-  toggleButton.classList.toggle('list-active')
+  toggleButton.classList.toggle('list-active');
+});
+
+//form action
+document.getElementById('username').addEventListener('focus', function() {
+  username.style.border = '2px solid #4D4D4D';
+});
+document.getElementById('usermail').addEventListener('focus', function() {
+  usermail.style.border = '2px solid #4D4D4D';
+});
+document.getElementById('message').addEventListener('focus', function() {
+  message.style.border = '2px solid #4D4D4D';
+});
+
+document.getElementById('btn').addEventListener('click', function(){
+  alert("შეტყობინება წარმატებით გაიგზავნა");
 })
